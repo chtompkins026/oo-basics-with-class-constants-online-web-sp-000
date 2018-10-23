e5e16d96c0f968fb3ade7fdcd95feb53d9e7a4ef
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-23 13:46:02 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,7 +13,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-    # create the writer for genre and add the logic for the class constant
+    # create the writer for brand and add the logic for the class constant
   def brand=(brand)
     @brand = brand
     BRAND << brand 
